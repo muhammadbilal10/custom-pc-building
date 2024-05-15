d425a8f52ad9f8d562cef126592a2be6700558b9
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2024-05-15 12:03:44 +0500

    update list page

--- src/app/(pc builder)/list/page.tsx
+++ src/app/(pc builder)/list/page.tsx
@@ -10,7 +10,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { Plus } from "lucide-react";
+import { CheckCircle, Plus } from "lucide-react";
 import Link from "next/link";
 
 const components = [
@@ -31,7 +31,7 @@ const components = [
 
 const productData = {
   image:
-    "https://www.cyberpowerpc.com/images/cs/HYTEY60/CS-450-172_400.png?v22", // Replace with actual image URL
+    "https://nzxt.com/assets/cms/34299/1664351911-n7-b650e-white-hero.png?ar64=MTox&auto=format&dpr=2&fit=crop&h=400&w=400", // Replace with actual image URL
   name: "AMD Ryzen 7 5800X Processor",
   brand: "AMD",
   specs: [
@@ -67,22 +67,51 @@ export default function ListPage() {
             Start by selecting the components you want to add to your build.
           </p>
         </div>
-        <div className="flex justify-between items-center">
-          <h2 className="text-4xl uppercase font-bold">Processor</h2>
-          <Link href="/product/processor">
-            <Plus size={48} />
-          </Link>
+        <div className="p-4 bg-[#43AA8B] text-white">
+          <div className="flex items-center space-x-2">
+            <CheckCircle className="h-5 w-5" />
+            <p className=" font-semibold">
+              Compatibility: No issues or incompatibilities found.
+            </p>
+          </div>
         </div>
-        <div className="max-w-md">
-          <PcComponetCard
-            title={productData.name}
-            imageUrl={productData.image}
-            buyLink={productData.productLink}
-            reviews={456}
-            ratingStars={4}
-            specs={["8 Cores", "16 Threads", "3.8 GHz (up to 4.7 GHz)"]}
-            os="Windows 10"
-          />
+        <div className="space-y-2">
+          <div className="flex justify-between items-center">
+            <h2 className="text-4xl uppercase font-bold">Processor</h2>
+            <Link href="/product/processor">
+              <Plus size={48} />
+            </Link>
+          </div>
+          <div className="max-w-md">
+            <PcComponetCard
+              title={productData.name}
+              imageUrl={productData.image}
+              buyLink={productData.productLink}
+              reviews={456}
+              ratingStars={4}
+              specs={["8 Cores", "16 Threads", "3.8 GHz (up to 4.7 GHz)"]}
+              os="Windows 10"
+            />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <div className="flex justify-between items-center">
+            <h2 className="text-4xl uppercase font-bold">MotherBoard</h2>
+            <Link href="/product/motherboard">
+              <Plus size={48} />
+            </Link>
+          </div>
+          <div className="max-w-md">
+            <PcComponetCard
+              title={productData.name}
+              imageUrl={productData.image}
+              buyLink={productData.productLink}
+              reviews={456}
+              ratingStars={4}
+              specs={["8 Cores", "16 Threads", "3.8 GHz (up to 4.7 GHz)"]}
+              os="Windows 10"
+            />
+          </div>
         </div>
       </div>
     </div>
