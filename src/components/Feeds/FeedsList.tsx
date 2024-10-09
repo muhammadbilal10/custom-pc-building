2daff58055019f8fb6f20b269e214d3349f9bb8e
Muhammad Bilal <muhdbilal81@gmail.com>
2024-10-09 08:58:34 +0500

    update feeds page

--- src/components/Feeds/FeedsList.tsx
+++ src/components/Feeds/FeedsList.tsx
@@ -17,7 +17,7 @@ export default async function FeedsList() {
   const builds = allBuilds.builds;
 
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {builds?.map((build) => (
         <PCBuildFeedItem
           key={build._id}
